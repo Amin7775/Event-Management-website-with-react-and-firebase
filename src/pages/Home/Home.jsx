b7da0b6635456cd8ea49000d63f2921767aca184
Amin7775 <amin.dpi24@gmail.com>
2023-10-09 17:44:54 +0600

    Added Upcoming Events Marquee section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,18 @@
 import Banner from "../../components/Header/Banner/Banner";
+import UpcomingEvents from "../../components/UpcominEvents/UpcomingEvents";
 
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            This is Home
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      {/* main section */}
+      <div className="max-w-[1440px] border-2 mx-auto">
+        {/* Upcoming Events */}
+        <UpcomingEvents></UpcomingEvents>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
